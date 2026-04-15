1da1098c2528a50183090c96c14f7312acfb8101
Henryk Pfeifer <110171639+henrykpfeifer@users.noreply.github.com>
2026-04-15 10:21:10 +0200

    Update genpasswds.cmd

--- genpasswds.cmd
+++ genpasswds.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0passwdgen.ps1" -Count 10 -OutputFile "%~dp0generated-password.txt"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0passwdgen.ps1"
